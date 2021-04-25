f2a4b814eacd2d8d024f6bfc8884398e28aae54e
iamayushdas <ayush24das@gmail.com>
2021-04-25 11:01:31 +0530

    app done

---
+++ covid-vue-tailwind/src/components/DataBoxes.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="grid md:grid-cols-2 gap-4">
+        <div class="shadow-md bg-green-100 p-10 text-center rounded">
+            <h3 class="text-3xl text-green-900 font-bold mb-4">Recovered</h3>
+
+            <div class="text-2xl mb-4">
+                <span class="font-bold">
+                    New:
+                </span>
+                {{ numberWithCommas(stats.NewRecovered) }}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">
+                    Total:
+                </span>
+                {{ numberWithCommas(stats.TotalRecovered) }}
+            </div>
+        </div>
+        <div class="shadow-md bg-blue-100 p-10 text-center rounded">
+            <h3 class="text-3xl text-blue-900 font-bold mb-4">cases</h3>
+
+            <div class="text-2xl mb-4">
+                <span class="font-bold">
+                    New:
+                </span>
+                {{ numberWithCommas(stats.NewConfirmed) }}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">
+                    Total:
+                </span>
+                {{ numberWithCommas(stats.TotalConfirmed) }}
+            </div>
+        </div>
+
+        <div class="shadow-md bg-red-200 p-10 text-center rounded">
+            <h3 class="text-3xl text-red-900 font-bold mb-4">Deceased</h3>
+
+            <div class="text-2xl mb-4">
+                <span class="font-bold">
+                    New:
+                </span>
+                {{ numberWithCommas(stats.NewDeaths) }}
+            </div>
+            <div class="text-2xl mb-4">
+                <span class="font-bold">
+                    Total:
+                </span>
+                {{ numberWithCommas(stats.TotalDeaths) }}
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'dataBoxes',
+    props: ['stats'],
+    methods: {
+        numberWithCommas(x) {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+        }
+    }
+
+}
+</script>
